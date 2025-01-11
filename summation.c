d12726c8c7aa5d5adbd38ef8ace01290fbefea96
Swarali789 <swarali2307.sante@gmail.com>
2025-01-11 15:20:49 +0530

    Update summation.c

--- summation.c
+++ summation.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    // Code to find addition of two numbers
+    // CODE TO FIND THE SUMMATION OF TWO NUMBERS
     float A,B,ADD;
     printf("Enter the two numbers : ");
     scanf("%f %f",&A,&B),
 
-    ADD = A + B;
+    ADD = A + B; // FOMULA FOR ADDITION
     printf("The addition of two numbers is : %f",ADD);
 
     return 0;
