2e602fc45776a996009cb6d8c613c6e92bb670d3
Swarali789 <swarali2307.sante@gmail.com>
2025-01-11 15:21:53 +0530

    Update volumeofsphere.c

--- volumeofsphere.c
+++ volumeofsphere.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter the radius of the sphere : ");
     scanf("%f",&r);
 
-    V = 4 * 3.142 * r * r * r / 3; //FORMULA FOR FINDING VOLUME
+    V = (4 * 3.142 * r * r * r) / 3; //FORMULA FOR FINDING VOLUME
     printf("The volume of the sphere of radius %f is : %f",r,V);
 
     return 0;
