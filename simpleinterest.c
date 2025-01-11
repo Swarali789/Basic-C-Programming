9b04673b6c30a78c1fa00c17ea08a37470e7e234
Swarali789 <swarali2307.sante@gmail.com>
2025-01-11 15:24:00 +0530

    Update simpleinterest.c

--- simpleinterest.c
+++ simpleinterest.c
@@ -2,11 +2,12 @@
 #include "simpleinterest.h"
 int main()
 {
+    // CODE TO FIND THE SIMPLE INTEREST
     float SI,P,R,T;
     printf("Enter the principal, rate and time respectively : ");
     scanf("%f %f %f",P,R,T);
 
-    SI = ( P * R * T ) / 100;
+    SI = ( P * R * T ) / 100; // FORMULA FOR SIMPLE INTEREST
     printf("The simple interest of the customer is : %f ", SI);
  
     return 0;
